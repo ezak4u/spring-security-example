fc80cd845780dc0d10d4f3576b7a2ecc2ab54e2e
Esakkimuthu Velraj <ezak4u@gmail.com>
2020-09-23 20:06:19 +0530

    spring config added with index page

---
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author ezak4
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter{
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/","/css/*","/js/*")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .httpBasic();
+    }
+}
