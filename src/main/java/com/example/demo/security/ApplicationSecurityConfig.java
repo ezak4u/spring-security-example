034c0e48a634090364f17a3a11d1c662d82fdad0
Esakkimuthu Velraj <ezak4u@gmail.com>
2020-09-23 20:21:00 +0530

    user details

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -7,6 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author ezak4
@@ -26,5 +30,15 @@ public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter{
             .authenticated()
             .and()
             .httpBasic();
+    }
+    
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.builder()
+                                    .username("ezak")
+                                    .password("password")
+                                    .roles("STUDENT") //ROLE_STUDENT
+                                    .build();
+        return new InMemoryUserDetailsManager(userDetails);
     }
 }
