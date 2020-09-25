8fe39b4fbec31e4f7f8321a1f506f5096bafbfa3
Esakkimuthu Velraj <ezak4u@gmail.com>
2020-09-25 14:59:31 +0530

    jwt token impl

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -7,23 +7,19 @@ import java.util.concurrent.TimeUnit;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.example.demo.auth.ApplicationUserService;
+import com.example.demo.jwt.JWTUsernamePasswordAuthenticationFilter;
+import com.example.demo.jwt.JwtTokenVerifier;
 
 /**
  * @author ezak4
@@ -53,6 +49,11 @@ public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter{
             //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
             //.and()
             .csrf().disable()
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .addFilter(new JWTUsernamePasswordAuthenticationFilter(authenticationManager()))
+            .addFilterAfter(new JwtTokenVerifier(), JWTUsernamePasswordAuthenticationFilter.class)
             .authorizeRequests()
             .antMatchers("/","/css/*","/js/*").permitAll()
             .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
@@ -61,10 +62,10 @@ public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter{
             //.antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(ApplicationUserPermission.COURSE_WRITE.getPermission())
             //.antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ApplicationUserRole.ADMIN.name(),ApplicationUserRole.ADMINTRAINEE.name())
             .anyRequest()
-            .authenticated()
-            .and()
+            .authenticated();
+            //.and()
             //.httpBasic();
-            .formLogin()
+            /*.formLogin()
                 .loginPage("/login").permitAll()
                 .usernameParameter("username")
                 .passwordParameter("password")
@@ -80,7 +81,7 @@ public class ApplicationSecurityConfig  extends WebSecurityConfigurerAdapter{
                 .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
                 .clearAuthentication(true)
                 .deleteCookies("JSESSIONID","remember-me")
-                .logoutSuccessUrl("/login");
+                .logoutSuccessUrl("/login");*/
     }
     
     @Override
